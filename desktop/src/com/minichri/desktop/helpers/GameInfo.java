2f0022406bb94a6f6701afd111eb7780db912d3c
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2018-04-26 19:12:03 +0200

    Added base for MapLoader, expanded TileType, added screenSize constants.

--- desktop/src/com/minichri/desktop/helpers/GameInfo.java
+++ desktop/src/com/minichri/desktop/helpers/GameInfo.java
@@ -3,4 +3,7 @@ package com.minichri.desktop.helpers;
 public class GameInfo {
 
     public static final int TILE_SIZE = 16;
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 800;
+
 }
--- desktop/src/com/minichri/desktop/helpers/TileType.java
+++ desktop/src/com/minichri/desktop/helpers/TileType.java
@@ -12,6 +12,24 @@ public enum TileType {
         this.color = color;
     }
 
+    /** Takes a color and returns a matching type. Returns null of non matches.
+     *  @param color a color to be matched.
+     *  @return a tileType based on the color given. Returns null if non matches. */
+    public static TileType getTypeFromColor(Color color){
+
+        if(color.equals(TileType.WHITE_SPACE.getColor()))
+            return WHITE_SPACE;
+        if(color.equals(TileType.GROUND.getColor()))
+            return GROUND;
+        else
+            return null;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    /** Color constants. */
     private static class Constants{
         static final Color WHITE_SPACE_COLOR = Color.valueOf("#"); //TODO
         static final Color GROUND_COLOR = Color.valueOf("#"); //TODO
