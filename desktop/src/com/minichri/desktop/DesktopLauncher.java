0409435d5f29d9601101b8cf53610b02efc0618d
Christopher Buchardt <cbucha17@student.aau.dk>
2018-04-26 18:32:16 +0200

    Generated project

---
+++ desktop/src/com/minichri/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.minichri.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.minichri.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
