7550eace2580ab17ce44a788410672e9581f319d
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2018-04-27 08:48:32 +0200

    bug

--- core/src/com/minichri/screens/GameScreen.java
+++ core/src/com/minichri/screens/GameScreen.java
@@ -77,8 +77,6 @@ public class GameScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         spriteBatch.begin();
-        spriteBatch.draw(img2,0,img.getHeight());
-        spriteBatch.draw(img,0,0);
 
         //TODO MIKKEL
         for(TextureObject textureObject : gameMap)
@@ -120,6 +118,5 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
         spriteBatch.dispose();
-        img.dispose();
     }
 }
