db5df646cea38dfb0cb3192348486799eba7fefe
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2018-04-28 10:49:20 +0200

    Collected Platform now travels faster and with item texture

--- core/src/com/minichri/Elements/CollectedPlatform.java
+++ core/src/com/minichri/Elements/CollectedPlatform.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import com.minichri.entity.TextureObject;
+import com.minichri.helpers.GameInfo;
 import com.minichri.helpers.TileType;
 
 public class CollectedPlatform extends TextureObject{
@@ -13,7 +14,7 @@ public class CollectedPlatform extends TextureObject{
     private boolean markedAsDeleted;
 
     public CollectedPlatform(World world, Vector2 pos, TileType tileType) {
-        super(world, pos, DEFAULT_DYNAMIC_BODYDEF, DEFAULT_STATIC_FIXTUREDEF, new TextureRegion(tileType.getBlockTexture()));
+        super(world, pos, DEFAULT_DYNAMIC_BODYDEF, DEFAULT_STATIC_FIXTUREDEF, new TextureRegion(tileType.getItemTexture()));
 
         this.tileType = tileType;
         body.getFixtureList().get(0).setSensor(true);
@@ -22,9 +23,14 @@ public class CollectedPlatform extends TextureObject{
     }
 
     public void render(SpriteBatch batch, float delta, Vector2 playerPos) {
-        super.render(batch, delta);
+        //super.render(batch, delta);
 
-        this.body.setLinearVelocity(new Vector2(playerPos).sub(this.body.getPosition()).scl(3));
+        this.body.setLinearVelocity(new Vector2(playerPos).sub(this.body.getPosition()).scl(6));
+
+        Vector2 pos = body.getPosition();
+        float width = texture.getRegionWidth() * GameInfo.PPM;
+        float height = texture.getRegionHeight() * GameInfo.PPM;
+        batch.draw(texture, pos.x - width/2, pos.y - height/2, width / 2f, height / 2f, width, height, 1, 1, body.getAngle());
     }
 
     public void markedAsDeleted(){
